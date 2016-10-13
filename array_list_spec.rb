0b4e9655624e560083b90f0337779af445ec930d
Neurodynamic <neurodynamicdev@gmail.com>
2016-10-13 15:39:46 -0400

    add spec skeleton

--- array_list_spec.rb
+++ array_list_spec.rb
@@ -1,5 +1,59 @@
 require_relative 'array_list'
 
 describe ArrayList do
+  describe '#new' do
+    it 'creates a new ArrayList with specified size' do
+      
+    end
+  end
 
+  describe '#add' do
+    it 'adds an element to the end of the list' do
+      
+    end
+    
+    it 'returns the element added' do
+      
+    end
+  end
+
+  describe '#get' do
+    it 'returns element at specified index' do
+      
+    end
+
+    it 'throws error if no element exists at specified index' do
+      
+    end
+  end
+
+  describe '#set' do
+    it 'replaces element at the specified index' do
+      
+    end
+    
+    it 'throws error if no element exists at specified index' do
+      
+    end
+  end
+
+  describe '#insert' do
+    it 'adds element with specified value at the specified index' do
+      
+    end
+
+    it 'pushes other elements right' do
+      
+    end
+    
+    it 'throws error if no element exists at specified index' do
+      
+    end
+  end
+
+  describe '#length' do
+    it 'returns number of items in list' do
+      
+    end
+  end
 end
